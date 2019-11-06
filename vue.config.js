0e01d061f5ad4d64df03a5dccc9e639c13aa54f0
guojing <guojing@A123>
2019-11-06 17:12:40 +0800

    修改格式

--- vue.config.js
+++ vue.config.js
@@ -43,15 +43,15 @@ module.exports = {
     ]
   },
   css: {
-      loaderOptions: {
-          // 给 stylus-loader 传递选项
-          stylus: {
-              import: [
-                  '~@/design/variable.styl',
-                  '~@/design/base.styl',
-                  '~@/design/mixin.styl'
-              ]
-          }
+    loaderOptions: {
+      // 给 stylus-loader 传递选项
+      stylus: {
+        import: [
+          '~@/design/variable.styl',
+          '~@/design/base.styl',
+          '~@/design/mixin.styl'
+        ]
       }
+    }
   }
 }
