8615a44a7922697c40c9ea5df651741a692d781b
guojing <guojing@A123>
2019-11-06 17:01:19 +0800

    合代码

--- vue.config.js
+++ vue.config.js
@@ -41,18 +41,17 @@ module.exports = {
     plugins: [
       definePlugin
     ]
+  },
+  css: {
+      loaderOptions: {
+          // 给 stylus-loader 传递选项
+          stylus: {
+              import: [
+                  '~@/design/variable.styl',
+                  '~@/design/base.styl',
+                  '~@/design/mixin.styl'
+              ]
+          }
+      }
   }
-module.exports = {
-    css: {
-        loaderOptions: {
-            // 给 stylus-loader 传递选项
-            stylus: {
-                import: [
-                    '~@/design/variable.styl',
-                    '~@/design/base.styl',
-                    '~@/design/mixin.styl'
-                ]
-            }
-        }
-    }
 }
