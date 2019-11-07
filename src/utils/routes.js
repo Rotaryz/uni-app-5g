302c4425d549612f8983ccb1c835d9755373b7b3
周旋 <zhouxuan@jkweixin.com>
2019-11-07 15:47:06 +0800

    修改模板

--- src/utils/routes.js
+++ src/utils/routes.js
@@ -1,29 +1,20 @@
 // PACKAGE_PATH
 const ACTIVITY = '/pages/package-activity'
-
-const ACTIVITY = '/pages/package-activity'
-
 const MAIN = '/pages/main'
 
 /* eslint-disable */
 export default {
   activity: {
     PACKAGE: ACTIVITY,
-    LIST:`${ACTIVITY}/list/list`,
-
-    DEMO:`${ACTIVITY}/demo/demo`,
-
-    // package-activity
-  },
-  activity: {
-    PACKAGE: ACTIVITY,
+    LIST: `${ACTIVITY}/list/list`,
+    DEMO: `${ACTIVITY}/demo/demo`,
     // package-activity
   },
   main: {
     PACKAGE: MAIN,
     INDEX: `${MAIN}/index/index`,
     MINE: `${MAIN}/mine/mine`,
-    LOGIN:`${MAIN}/login/login`,
+    LOGIN: `${MAIN}/login/login`,
     SHOPPING_CART: `${MAIN}/shopping-cart/shopping-cart`,
     CATEGORY: `${MAIN}/category/category`,
     // main
