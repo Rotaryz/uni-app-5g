45587c3fbb567a488aed6dbc1bed9869e6f4b057
周旋 <zhouxuan@jkweixin.com>
2019-11-07 17:49:27 +0800

    修改单位配置

--- src/pages/package-activity/demo/demo.vue
+++ src/pages/package-activity/demo/demo.vue
@@ -1,6 +1,16 @@
 <template>
   <div class="demo">
-    demo
+    <div v-for="(item, index) in list" :key="index" class="item">
+      <text class="red">{{page}}</text>
+      <div>{{item}}</div>
+      <div>{{item}}</div>
+      <div>{{item}}</div>
+      <div>{{item}}</div>
+      <div>{{item}}</div>
+      <div>{{item}}</div>
+      <div>{{item}}</div>
+      <div>{{item}}</div>
+    </div>
   </div>
 </template>
 
@@ -14,14 +24,33 @@
     name: PAGE_NAME,
     data() {
       return {
+        list: [],
+        page: 1
+      }
+    },
+    onLoad() {
+      this.getMore()
+    },
+    onReachBottom() {
+      this.page++
+      this.getMore()
+    },
+    methods: {
+      getMore() {
+        for (let i = 0; i < 10; i++) {
+          this.list.push(`发昆仑山覅按时来开发哈拉开始放假了开始发个图人${i}`)
+        }
       }
     }
   }
 </script>
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
-  /*@import "~@design"*/
 
-  .demo
-    width: 100%
+  .item
+    display: block
+    text-align: center
+
+  .red
+    color: red
 </style>
