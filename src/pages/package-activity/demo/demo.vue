ee5d6b196497b61a6e53f31afd07f4ba273b68e9
周旋 <zhouxuan@jkweixin.com>
2019-11-07 15:45:17 +0800

    修改模板

---
+++ src/pages/package-activity/demo/demo.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="demo">
+    demo
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  // import * as Helpers from './helpers'
+  // import API from '@api'
+
+  const PAGE_NAME = 'DEMO'
+
+  export default {
+    name: PAGE_NAME,
+    data() {
+      return {
+      }
+    }
+  }
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+  /*@import "~@design"*/
+
+  .demo
+    width: 100%
+</style>
